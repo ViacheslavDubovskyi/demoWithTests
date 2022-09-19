8a7e67b539af782467385055b212d4aba534b966
Viacheslav Dubovskyi <architect1slava@gmail.com>
2022-09-19 19:30:14 +0300

    init controller dto

--- src/main/java/com/example/demowithtests/web/Controller.java
+++ src/main/java/com/example/demowithtests/web/Controller.java
@@ -1,12 +1,19 @@
 package com.example.demowithtests.web;
 
 import com.example.demowithtests.domain.Employee;
+import com.example.demowithtests.dto.EmployeeDto;
+import com.example.demowithtests.dto.EmployeeReadDto;
 import com.example.demowithtests.service.Service;
+import com.example.demowithtests.util.config.EmployeeConverter;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -15,115 +22,83 @@ import java.util.List;
 public class Controller {
 
     private final Service service;
+    private final EmployeeConverter converter;
 
-    /**
-     * This method saves new Employee object
-     *
-     * @param employee The name of the object of the Employee which we are saving
-     * @return New Employee
-     */
     @PostMapping("/users")
     @ResponseStatus(HttpStatus.CREATED)
-    public Employee saveEmployee(@RequestBody Employee employee) {
-        return service.create(employee);
+    @Operation(summary = "This is endpoint to add a new employee.", description = "Create request to add a new employee.", tags = {"Employee"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "CREATED. The new employee is successfully created and added to database."),
+            @ApiResponse(responseCode = "400", description = "Invalid input"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND. Specified employee request not found."),
+            @ApiResponse(responseCode = "409", description = "Employee already exists")})
+    public EmployeeDto saveEmployee(@RequestBody @Valid EmployeeDto requestForSave) {
+
+        var employee = converter.getMapperFacade().map(requestForSave, Employee.class);
+        var dto = converter.toDto(service.create(employee));
+
+        return dto;
     }
 
-    /**
-     * This method returns all the employees in the table
-     *
-     * @return List of all Employees
-     */
     @GetMapping("/users")
     @ResponseStatus(HttpStatus.OK)
     public List<Employee> getAllUsers() {
         return service.getAll();
     }
-
-    /**
-     * This method searches the Employee with specified id
-     *
-     * @param id The id of the certain employee
-     * @return Employee
-     */
+    //Получения юзера по id
     @GetMapping("/users/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Employee getEmployeeById(@PathVariable Integer id) {
-
-        Employee employee = service.getById(id);
-        return employee;
+    @Operation(summary = "This is endpoint returned a employee by his id.", description = "Create request to read a employee by id", tags = {"Employee"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "OK. pam pam param."),
+            @ApiResponse(responseCode = "400", description = "Invalid input"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND. Specified employee request not found."),
+            @ApiResponse(responseCode = "409", description = "Employee already exists")})
+    public EmployeeReadDto getEmployeeById(@PathVariable Integer id) {
+        //log.debug("getEmployeeById() Controller - start: id = {}", id);
+        var employee = service.getById(id);
+        //log.debug("getById() Controller - to dto start: id = {}", id);
+        var dto = converter.toReadDto(employee);
+        //log.debug("getEmployeeById() Controller - end: name = {}", dto.name);
+        return dto;
     }
 
-    /**
-     * This method updates already existing Employee
-     *
-     * @param id       The id of the certain employee
-     * @param employee The employee we want to update
-     * @return Employee
-     */
     @PutMapping("/users/{id}")
     @ResponseStatus(HttpStatus.OK)
     public Employee refreshEmployee(@PathVariable("id") Integer id, @RequestBody Employee employee) {
         return service.updateById(id, employee);
     }
 
-    /**
-     * This method deletes certain employee from the table
-     *
-     * @param id The id of the certain employee
-     */
     @PatchMapping("/users/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void removeEmployeeById(@PathVariable Integer id) {
         service.removeById(id);
     }
 
-    /**
-     * This method removes all employees from the table
-     */
     @DeleteMapping("/users")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void removeAllUsers() {
         service.removeAll();
     }
 
-    /**
-     * This method searches all employees with the certain name
-     *
-     * @param name The name of request parameter
-     * @return List of Employees with specified name
-     */
     @GetMapping(value = "/users", params = {"name"})
     @ResponseStatus(HttpStatus.OK)
     public List<Employee> getAllByName(@RequestParam(value = "name") String name) {
         return service.findAllByName(name);
     }
 
-    /**
-     * This method searches all employees with the filled field phoneNumber
-     *
-     * @return List of Employees
-     */
     @GetMapping(value = "/users/phone_number")
     @ResponseStatus(HttpStatus.OK)
     public List<Employee> getUsersWithPhoneNumber() {
         return service.findUsersWithPhoneNumber();
     }
 
-    /**
-     * This method searches users with no email
-     * @return List of Employees
-     */
     @GetMapping("/users/no_email")
     @ResponseStatus(HttpStatus.OK)
     public List<Employee> generateEmail() {
         return service.findRecordsWhereEmailNull();
     }
 
-    /**
-     * This method find users by field "country"
-     * @param country This is country of employee
-     * @return List of Employees
-     */
     @GetMapping(value = "/users", params = {"country"})
     @ResponseStatus(HttpStatus.OK)
     public List<Employee> getUsersByCountry(@RequestParam(value = "country") String country) {
