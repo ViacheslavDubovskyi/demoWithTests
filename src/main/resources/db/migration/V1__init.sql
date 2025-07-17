a0ceb780e8eb6f3dead5d2eab5c8a173f529d217
Viacheslav Dubovskyi <architect1slava@gmail.com>
2025-07-17 11:41:48 +0300

    update sql script

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,5 +1,11 @@
+create table public.salary (
+                               id serial primary key,
+                               commission_rate double precision,
+                               hourly_rate integer
+);
+
 create table public.users (
-                              id integer primary key not null default nextval('users_id_seq'::regclass),
+                              id serial primary key,
                               name character varying(255),
                               country character varying(255),
                               email character varying(255),
@@ -12,7 +18,7 @@ create table public.users (
 );
 
 create table public.addresses (
-                                  id bigint primary key not null default nextval('addresses_id_seq'::regclass),
+                                  id bigserial primary key,
                                   address_has_active boolean,
                                   city character varying(255),
                                   country character varying(255),
@@ -23,9 +29,4 @@ create table public.addresses (
                                       match simple on update no action on delete no action
 );
 
-create table public.salary (
-                               id integer primary key not null default nextval('salary_id_seq'::regclass),
-                               commission_rate double precision,
-                               hourly_rate integer
-);
 
